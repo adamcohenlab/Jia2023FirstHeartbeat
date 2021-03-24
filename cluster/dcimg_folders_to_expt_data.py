906b91dadd68223834bf82e5395f082e43e63230
Bill Jia <billjia@g.harvard.edu>
2021-03-24 09:30:49 -0400

    Fix expt_data script

--- cluster/dcimg_folders_to_expt_data.py
+++ cluster/dcimg_folders_to_expt_data.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import numpy as np
 import pandas as pd
-from parse import parse
+from parse import search
 
 parser = argparse.ArgumentParser()
 parser.add_argument("rootpath", type=str)
@@ -14,8 +14,11 @@ entries = []
 for folder in os.listdir(rootpath):
     if os.path.isdir(os.path.join(rootpath, folder)):
         print(folder)
-        res = parse("{hh:2d}{mm:2d}{ss:2d}_p1_t1", folder)
-        entries.append(("%2d:%2d:%2d" % (res['hh'], res['mm'], res['ss']), folder))
+        # res = parse("It's {}, I love it!", "It's spam, I love it!")
+        res = search("{hh:2d}{mm:2d}{ss:2d}_p1", folder)
+        # print(res)
+        entries.append(("%02d:%02d:%02d" % (res['hh'], res['mm'], res['ss']), folder))
 
+print(entries)
 df = pd.DataFrame(entries, columns=["start_time", "file_name"])
-df.to_csv(os.path.join(rootpath,"experiment_data.csv"))
+df.to_csv(os.path.join(rootpath,"experiment_data.csv"),index=False)
