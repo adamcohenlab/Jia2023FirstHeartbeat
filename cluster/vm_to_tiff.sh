306368ed6cbb7252a3362c6b4034a5e60dd5c12d
Bill Jia <billjia@g.harvard.edu>
2021-03-24 11:40:46 -0400

    More bash script

--- cluster/vm_to_tiff.sh
+++ cluster/vm_to_tiff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -n 1                # Number of cores (-n)
 #SBATCH -N 1                # Ensure that all cores are on one Node (-N)
-#SBATCH -t 0-00:05          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=6000          # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=8000          # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 module load matlab/R2018b-fasrc01
