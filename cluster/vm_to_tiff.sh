4b9ae050ab3c4028b625ae4bdee21bc2844adbee
Bill Jia <billjia@g.harvard.edu>
2021-03-24 11:29:38 -0400

    Finalize batch script

--- cluster/vm_to_tiff.sh
+++ cluster/vm_to_tiff.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -n 1                # Number of cores (-n)
 #SBATCH -N 1                # Ensure that all cores are on one Node (-N)
-#SBATCH -t 0-00:30          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-00:05          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=4000          # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=6000          # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 module load matlab/R2018b-fasrc01
-matlab -nojvm -nosplash -nodesktop -r "addpath('/n/cohen_lab/Lab/Computer Code/Image Processing/');batch_vm_to_tiff('$1','$2', 1, 1);"
+matlab -nojvm -nosplash -nodesktop -nodisplay -r "addpath('/n/cohen_lab/Lab/Computer Code/Image Processing/');batch_vm_to_tiff('$1','$2', 1, 1);"
