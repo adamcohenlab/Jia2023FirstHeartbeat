5833e39ff6b67ef375f90e97a7e81657a4658d9c
Bill Jia <bjia@holy7c26402.rc.fas.harvard.edu>
2021-04-29 08:28:35 -0400

    Stuff

--- cluster/clicky_calcium.sh
+++ cluster/clicky_calcium.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -n 1                # Number of cores (-n)
 #SBATCH -N 1                # Ensure that all cores are on one Node (-N)
-#SBATCH -t 0-00:15          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p shared   # Partition to submit to
+#SBATCH -t 0-00:20          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p serial_requeue   # Partition to submit to
 #SBATCH --mem=16000          # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
--- cluster/vm_to_tiff.sh
+++ cluster/vm_to_tiff.sh
@@ -2,8 +2,8 @@
 #SBATCH -n 1                # Number of cores (-n)
 #SBATCH -N 1                # Ensure that all cores are on one Node (-N)
 #SBATCH -t 0-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p shared   # Partition to submit to
-#SBATCH --mem=16000          # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -p serial_requeue   # Partition to submit to
+#SBATCH --mem=20000          # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 module load matlab/R2018b-fasrc01
