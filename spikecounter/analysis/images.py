dd35c2c4da5ec1cd9c54022293c853781397cfdd
Bill Jia <bjia@holy7c26602.rc.fas.harvard.edu>
2021-07-13 11:39:30 -0400

    Add SNAPT algorithm

--- spikecounter/analysis/images.py
+++ spikecounter/analysis/images.py
@@ -1,12 +1,13 @@
 import numpy as np
 import scipy.ndimage as ndi
-from scipy import signal, stats
+from scipy import signal, stats, interpolate, optimize
 import matplotlib.pyplot as plt
 from . import traces
+from .. import utils
 from sklearn.decomposition import PCA
 
 def refine_segmentation_pca(img, mask, n_components=10, threshold_percentile=70):
-    """ Refine manual segmentation to localize the heart using PCA.
+    """ Refine manual segmentation to localize the heart using PCA assuming that transients are the largest fluctuations present.
     Returns cropped region images and masks for each unique region in the global image mask.
     
     """
@@ -90,7 +91,6 @@ def spike_mask_to_stim_index(spike_mask):
     stim_end = np.argwhere(diff_mask==-1).ravel()+1
     return stim_end
 
-
 def image_to_trace(img, mask = None):
     """ Convert part of an image to a trace according to a mask
     
@@ -101,6 +101,21 @@ def image_to_trace(img, mask = None):
         masked_img = np.ma.masked_array(img, mask=~mask)
         trace = masked_img.mean(axis=(1,2))
     return trace
+
+def interpolate_invalid_values(img, mask):
+    """ Interpolate invalid values pixelwise
+    """
+    xs = np.arange(img.shape[0])[~mask]
+    invalid_filled = np.copy(img)
+    for i in range(img.shape[1]):
+        for j in range(img.shape[2]):
+            ys = img[:,i,j][~mask]
+            interp_f = interpolate.interp1d(xs, ys, kind="cubic", fill_value="extrapolate")
+            missing_xs = np.argwhere(mask).ravel()
+            missing_ys = interp_f(missing_xs)
+            invalid_filled[mask,i,j] = missing_ys
+    return invalid_filled
+    
 
 def plot_image_mean_and_stim(img, mask=None, style="line", duration=0, fs=1):
     """ Plot mean of image (mask optional) and mark points where the image was stimulated
@@ -125,6 +140,89 @@ def plot_image_mean_and_stim(img, mask=None, style="line", duration=0, fs=1):
 def background_subtract(img, dark_level=100):
     return img - dark_level
 
+def get_spike_kernel(img, kernel_length, nbefore, peak_prominence, savgol_length=51):
+    """ Estimate a temporal spike kernel by doing naive peak detection and selecting a temporal window. Pick a subsample with the largest peak amplitudes after smoothing (presumably largest SNR) and average.
+    
+    Returns:
+        kernel - temporal kernel for the spike
+        kernel_hits - which pixels in the image were selected to be averaged into the kernel
+    """
+    kernel_candidates = []
+    height = img.shape[1]
+    width = img.shape[2]
+    kernel_hits = np.zeros((height, width), dtype=bool)
+
+    for i in range(height):
+        for j in range(width):
+            trace_smoothed = signal.savgol_filter(img[:,i,j], savgol_length, 2)
+            px_peaks, _ = signal.find_peaks(trace_smoothed, prominence=peak_prominence)
+            if len(px_peaks) > 1:
+                plt.plot(trace_smoothed)
+                plt.plot(px_peaks, trace_smoothed[px_peaks], "rx")
+                raise ValueError("More than one candidate peak detected")
+            elif len(px_peaks) == 1:
+                kernel_hits[i,j] = True
+                pidx = px_peaks[0]
+                if pidx < nbefore:
+                    xs = np.arange(nbefore-pidx, kernel_length)
+                    ys = img[:pidx-nbefore+kernel_length, i,j]
+                    interpf = interpolate.interp1d(xs, ys, fill_value="extrapolate")
+                    ktrace = interpf(np.arange(kernel_length))
+                elif pidx-nbefore+kernel_length > img.shape[0]:
+                    xs = np.arange(img.shape[0] - (pidx-nbefore))
+                    ys = img[pidx-nbefore:,i,j]
+                    interpf = interpolate.interp1d(xs, ys, fill_value="extrapolate")
+                    ktrace = interpf(np.arange(kernel_length))
+                else:
+                    ktrace = img[pidx-nbefore:pidx-nbefore+kernel_length,i,j]
+                ktrace = (ktrace - np.min(ktrace))/(np.max(trace_smoothed) - np.min(trace_smoothed))
+                kernel_candidates.append(ktrace)
+    kernel = np.zeros(img.shape[0])
+    kernel[:kernel_length] = np.mean(np.array(kernel_candidates[:]), axis=0)
+    kernel[kernel_length:] = kernel[kernel_length-1]
+    return kernel, kernel_hits
+
+def snapt(img, kernel):
+    """ Run SNAPT fitting algorithm (Hochbaum et al. Nature Methods 2014)
+    """
+    height = img.shape[1]
+    width = img.shape[2]
+    beta = np.zeros((height, width, 4))
+    error_det = np.zeros((height, width))
+    failed_counter = 0
+    for i in range(height):
+        for j in range(width):
+            try:
+                popt, pcov = optimize.curve_fit(utils.shiftkern, kernel, img[:,i,j], p0=[0.05,1,1,0], absolute_sigma=True)
+                beta[i,j,:] = popt
+                error_det[i,j] = np.linalg.det(pcov)
+            except Exception as e:
+                print("(%d, %d) %s" % (i,j, str(e)))
+                beta[i,j,:] = np.nan*np.ones(4)
+                error_det[i,j] = np.nan
+                failed_counter += 1
+    print("%d/%d pixels failed to fit (%.2f %%)" % (failed_counter, height*width, failed_counter/(height*width)*100))
+    return beta, error_det
+
+def correct_photobleach(img, mask=None, method="localmin", nsamps=51):
+    """ Perform photobleach correction on each pixel in an image
+    """
+    if method == "linear":
+        corrected_img = np.zeros_like(img)
+        for y in range(img.shape[1]):
+            for x in range(img.shape[2]):
+                corrected_trace, _ = traces.correct_photobleach(img[:,y,x])
+                corrected_img[:, y,x] = corrected_trace
+                
+    elif method == "localmin":
+        mean_trace = image_to_trace(img, mask)
+        _, pbleach = traces.correct_photobleach(mean_trace, method=method, nsamps=nsamps)
+        corrected_img = np.divide(img, pbleach[:,np.newaxis,np.newaxis])
+        print(corrected_img.shape)
+    else:
+        raise ValueError("Not Implemented")
+        
+    return corrected_img
 
 def get_image_dFF(img, baseline_percentile=10):
     """ Convert a raw image into dF/F
@@ -215,3 +313,7 @@ def analyze_wave_prop(masked_image, mask, nbins=16):
     hist, _ = np.histogram(angle_masked, bins=nbins, range=(-np.pi/2, np.pi/2))
     entr = stats.entropy(hist)
     return mean_magnitude, var_magnitude, median_magnitude, direction, entr
+
+def fill_nans_with_neighbors(img):
+    isnan = np.isnan(img)
+    
